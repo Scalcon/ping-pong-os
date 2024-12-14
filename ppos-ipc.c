868e0335ebb7f8976c3c38f0e37944f176c37421
RodrigoWPoli <rodtbag@gmail.com>
2024-12-14 11:23:02 -0300

    umas merdas ai

--- ppos-ipc.c
+++ ppos-ipc.c
@@ -8,16 +8,24 @@
 int sem_create(semaphore_t *s, int value)
 {
     if (s == NULL)  return -1;
-    PPOS_PREEMPT_DISABLE; // disable preemption to stop interruptions
+    PPOS_PREEMPT_DISABLE; // disable preemption to stop race conditions
     s->counter = value;
     s->queue = NULL;
     s->active = 1;
-    PPOS_PREEMPT_ENABLE; // enable preemption
+    PPOS_PREEMPT_ENABLE; // enable preemption again
     return 0;
 }
 
 int sem_down(semaphore_t *s)
 {
+    if (s == NULL || !s->active) {
+        return -1;
+    }
+
+    PPOS_PREEMPT_DISABLE;
+    s->counter--;
+    
+    
     return 0;
 }
 
@@ -26,12 +34,21 @@ int sem_up(semaphore_t *s)
     return 0;
 }
 
+
+// destroy the semaphore and wake up all tasks that are waiting for it
 int sem_destroy(semaphore_t *s)
 {
-    if (s == NULL || !(s->active)) {
+    if (s == NULL || !s->active) {
         return -1;
     }
+    PPOS_PREEMPT_DISABLE;
+    s->active = 0;
     
+    while (s->queue != NULL){
+        task_resume(s->queue);
+    }
+
+    PPOS_PREEMPT_ENABLE; 
 
     return 0;
 }
